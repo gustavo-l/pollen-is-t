706c1042afde6ba05ba6f65da777b3945c2273bf
Marcelo Fortunato <marcelo.fortunato@pdmfc.com>
2018-09-29 11:47:30 +0100

    changed lib template

--- src/main/main.component.jsx
+++ src/main/main.component.jsx
@@ -3,11 +3,10 @@ import React from 'react'
 import { Routes } from './main.routes'
 import { withRouter } from 'react-router'
 const Main = props => (
-    <div>
-        <h1>Heading</h1>
-        <div>
-            <Routes />
-        </div>
-    </div>
+	<div>
+		<div>
+			<Routes />
+		</div>
+	</div>
 )
 export default withRouter(Main)
