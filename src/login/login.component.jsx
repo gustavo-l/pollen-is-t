a0b250b984b4f15dee9edd0b848355e00306e91a
Logitao <gmdramos@gmail.com>
2018-09-30 14:26:41 -0300

    [fixes] - fixing some bugs

--- src/login/login.component.jsx
+++ src/login/login.component.jsx
@@ -13,52 +13,52 @@ import { validate } from './login.validation.js'
 import './login.styles.scss'
 
 const LoginForm = ({ handleSubmit, submitting, error }) => (
-	<div className="panel">
-		<form onSubmit={handleSubmit(login)}>
-			<h2>Entre para continuar</h2>
-			<Field
-				type="text"
-				name="username"
-				label="Endereço de e-mail"
-				component={RenderInput}
-			/>
-			<Field
-				type="password"
-				name="password"
-				label="Palavra-passe"
-				component={RenderInput}
-			/>
-			<Button type="submit" disabled={submitting} fullwidth inform>
-				Entrar
-			</Button>
-			<div className="rememberme-container">
-				<CheckBox className="rememberme-item" label="Lembrar" />
-				<a href="#">Precisa de ajuda?</a>
-			</div>
-			<div className="separator">
-				<span className="separator-text">ou continue com</span>
-				<hr />
-			</div>
-			<div className="row">
-				<div className="col-6">
-					{error && <strong>{error}</strong>}
-					<Button disabled={submitting} facebook fullwidth inform>
-						Facebook
-					</Button>
-				</div>
-				<div className="col-6">
-					<Button disabled={submitting} google fullwidth inform>
-						Google
-					</Button>
-				</div>
-			</div>
-		</form>
-	</div>
+    <div className="panel">
+        <form onSubmit={handleSubmit(login)}>
+            <h2>Entre para continuar</h2>
+            <Field
+                type="text"
+                name="user"
+                label="Endereço de e-mail"
+                component={RenderInput}
+            />
+            <Field
+                type="password"
+                name="password"
+                label="Palavra-passe"
+                component={RenderInput}
+            />
+            <Button type="submit" disabled={submitting} fullwidth inform>
+                Entrar
+            </Button>
+            <div className="rememberme-container">
+                <CheckBox className="rememberme-item" label="Lembrar" />
+                <a href="#">Precisa de ajuda?</a>
+            </div>
+            <div className="separator">
+                <span className="separator-text">ou continue com</span>
+                <hr />
+            </div>
+            <div className="row">
+                <div className="col-6">
+                    {error && <strong>{error}</strong>}
+                    <Button disabled={submitting} facebook fullwidth inform>
+                        Facebook
+                    </Button>
+                </div>
+                <div className="col-6">
+                    <Button disabled={submitting} google fullwidth inform>
+                        Google
+                    </Button>
+                </div>
+            </div>
+        </form>
+    </div>
 )
 
 export default withRouter(
-	reduxForm({
-		form: 'login',
-		validate
-	})(LoginForm)
+    reduxForm({
+        form: 'login',
+        validate
+    })(LoginForm)
 )
