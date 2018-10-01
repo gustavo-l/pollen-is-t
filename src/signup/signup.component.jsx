fd7333b3e947f96310a84e1a90c5ba840650c549
Marcelo Fortunato <marcelo.fortunato@pdmfc.com>
2018-10-01 11:46:17 +0100

    ajustes de layout

--- src/signup/signup.component.jsx
+++ src/signup/signup.component.jsx
@@ -11,56 +11,40 @@ import { signup } from './signup.async'
 import './signup.styles.scss'
 
 const LoginForm = ({ handleSubmit, submitting }) => (
-    <div className="panel">
-        <form onSubmit={handleSubmit(signup)}>
-            <div>
-                <div className="signup-alt">
-                    Cadastre-se com <a href="#/user">Facebook</a> ou{' '}
-                    <a href="" target="_blank">
-                        Google
-                    </a>
-                </div>
-                <div className="separator">
-                    <span className="separator-text">OU</span>
-                    <hr />
-                </div>
-                <Field
-                    name="firstname"
-                    label="Primeiro Nome"
-                    component={RenderInput}
-                />
-                <Field
-                    name="lastname"
-                    label="Último nome"
-                    component={RenderInput}
-                />
-                <Field name="email" label="Email" component={RenderInput} />
-
-                <Field
-                    name="password"
-                    label="Criar uma senha"
-                    type="password"
-                    component={RenderInput}
-                />
-
-                <Field name="user" label="Usuário" component={RenderInput} />
-                <Button
-                    type="submit"
-                    large
-                    disabled={submitting}
-                    fullwidth
-                    inform
-                >
-                    Cadastrar
-                </Button>
-            </div>
-        </form>
-    </div>
+	<div className="panel">
+		<form onSubmit={handleSubmit(signup)}>
+			<div>
+				<div className="signup-alt">
+					Cadastre-se com <a href="#/user">Facebook</a> ou{' '}
+					<a href="" target="_blank">
+						Google
+					</a>
+				</div>
+				<div className="separator">
+					<span className="separator-text">ou</span>
+					<hr />
+				</div>
+				<Field name="email" label="Endereço de email" component={RenderInput} />
+				<Field name="firstname" label="Primeiro nome" component={RenderInput} />
+				<Field name="lastname" label="Último nome" component={RenderInput} />
+				<Field name="user" label="Usuário" component={RenderInput} />
+				<Field
+					name="password"
+					label="Criar uma senha"
+					type="password"
+					component={RenderInput}
+				/>
+				<Button type="submit" small disabled={submitting} fullwidth inform>
+					Cadastrar
+				</Button>
+			</div>
+		</form>
+	</div>
 )
 
 export default withRouter(
-    reduxForm({
-        form: 'signup',
-        validate
-    })(LoginForm)
+	reduxForm({
+		form: 'signup',
+		validate
+	})(LoginForm)
 )
