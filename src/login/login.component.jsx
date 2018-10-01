3add8bbab625fbdf455d3f970ce86ba2884c4eec
Marcelo Fortunato <marcelo.fortunato@pdmfc.com>
2018-10-01 10:19:29 +0100

    ajustes padding class small

--- src/login/login.component.jsx
+++ src/login/login.component.jsx
@@ -13,55 +13,55 @@ import { validate } from './login.validation.js'
 import './login.styles.scss'
 
 const LoginForm = ({ handleSubmit, submitting, error }) => (
-    <div className="panel">
-        <form onSubmit={handleSubmit(login)}>
-            <h2>Entre para continuar</h2>
-            <Field
-                type="text"
-                name="user"
-                small
-                label="Endereço de e-mail"
-                component={RenderInput}
-            />
-            <Field
-                type="password"
-                name="password"
-                small
-                label="Palavra-passe"
-                component={RenderInput}
-            />
-            <Button type="submit" small disabled={submitting} fullwidth inform>
-                Entrar
-            </Button>
-            <div className="rememberme-container">
-                <CheckBox className="flexitem-1" label="Lembrar" />
-                <a href="#">Precisa de ajuda?</a>
-            </div>
-            <div className="separator">
-                <span className="separator-text">ou continue com</span>
-                <hr />
-            </div>
+	<div className="panel">
+		<form onSubmit={handleSubmit(login)}>
+			<h2>Entre para continuar</h2>
+			<Field
+				type="text"
+				name="user"
+				small
+				label="Endereço de e-mail"
+				component={RenderInput}
+			/>
+			<Field
+				type="password"
+				name="password"
+				small
+				label="Palavra-passe"
+				component={RenderInput}
+			/>
+			<Button type="submit" small disabled={submitting} fullwidth inform>
+				Entrar
+			</Button>
+			<div className="rememberme-container">
+				<CheckBox className="flexitem-1" label="Lembrar" />
+				<a href="#">Precisa de ajuda?</a>
+			</div>
+			<div className="separator">
+				<span className="separator-text">ou continue com</span>
+				<hr />
+			</div>
 
-            <div className="register-options-container">
-                <div className="item-margin flexitem-1">
-                    {error && <strong>{error}</strong>}
-                    <Button disabled={submitting} facebook fullwidth inform>
-                        Facebook
-                    </Button>
-                </div>
-                <div className="item-margin flexitem-1">
-                    <Button disabled={submitting} google fullwidth inform>
-                        Google
-                    </Button>
-                </div>
-            </div>
-        </form>
-    </div>
+			<div className="register-options-container">
+				<div className="item-margin flexitem-1">
+					{error && <strong>{error}</strong>}
+					<Button disabled={submitting} facebook small fullwidth inform>
+						Facebook
+					</Button>
+				</div>
+				<div className="item-margin flexitem-1">
+					<Button disabled={submitting} google small fullwidth inform>
+						Google
+					</Button>
+				</div>
+			</div>
+		</form>
+	</div>
 )
 
 export default withRouter(
-    reduxForm({
-        form: 'login',
-        validate
-    })(LoginForm)
+	reduxForm({
+		form: 'login',
+		validate
+	})(LoginForm)
 )
